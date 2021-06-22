f00726d543f887a45b577535b5388d40c886fba1
swa07016 <swa07016@naver.com>
2021-06-22 13:25:03 +0000

    Create Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer =  (props) => {
+	
+	return (
+		<div style={{
+				width: '100%',
+				height: '6rem',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}>
+			<div>Copyright © swa07016 All rights reserved</div>
+		</div>
+	);
+}
+
+export default Footer;
--- src/components/page/MainPage.jsx
+++ src/components/page/MainPage.jsx
@@ -5,15 +5,11 @@ import { Benefit } from '../benefit'
 import { Tmo } from '../tmo'
 import { Px } from '../px'
 import { Hotel } from '../hotel'
+import Footer from '../Footer';
 
-import { About } from '../about'
-import { Services } from '../services'
-import { Gallery } from '../gallery'
-import { Testimonials } from '../testimonials'
-import { Team } from '../Team'
-import { Contact } from '../contact'
 import JsonData from '../../data/data.json'
 import SmoothScroll from 'smooth-scroll'
+
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -34,7 +30,7 @@ const MainPage = () => {
 	  <Tmo data={landingPageData.Features} />
 	  <Px data={landingPageData.Features} />
 	  <Hotel data={landingPageData.Features} />
-	  {/* footer area */}
+	  <Footer />
     </div>
   )
 }
